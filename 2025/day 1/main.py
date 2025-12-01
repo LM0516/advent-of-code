67c8609358d4cbb773a5b223c5549486e348465b
Lorenzo Minuz <minuzlorenzo@gmail.com>
2025-12-01 16:20:51 +0100

    completed part one day one

--- 2025/day 1/main.py
+++ 2025/day 1/main.py
@@ -11,27 +11,27 @@ def rotations(file_name):
     return split_data
 
 
+def part_one(data, value, count):
+    for i in range(len(data)):
+        match data[i][0]:
+            case "L":
+                value = (value - data[i][1]) % 100
+            case "R":
+                value = (value + data[i][1]) % 100
+
+        if value == 0:
+            count += 1
+
+    print(count)
+
+
 def main():
     file_name = "rotations.txt"
     data = rotations(file_name)
     value = 50
     count = 0
 
-    for i in range(len(data)):
-        if value == 0:
-            count += 1
-
-        match data[i][0]:
-            case "L":
-                value -= data[i][1]
-                if value < 0:
-                    value = 100 + value
-            case "R":
-                value += data[i][1]
-                if value > 99:
-                    value = value - 100
-
-    print(count)
+    part_one(data, value, count)
 
 
 if __name__ == "__main__":
